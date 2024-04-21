0a64aabb58e0538b3132eac238fc7d91327ea3c7
car-junior <carlosilva.dev@gmail.com>
2024-04-21 09:32:39 -0300

    feat: Ajustes em classe que cria dataset a partir de arquivos csv - Adiciona encoding de leitura dos arquivos csv (cp1252) - Cria constantes com configurações padrões para leitura dos arquivos csv - Ajusta lógica de leitura e agrupamento dos arquivos - Move arquivos de registros climáticos para uma pasta em comum

--- scripts/analise_preliminar.py
+++ scripts/analise_preliminar.py
@@ -3,6 +3,4 @@ import matplotlib as mt
 import os
 from misc import Dataframes
 
-arquivo = r"C:\Users\muril\OneDrive\Área de Trabalho\Desafio de Dados - PUC 2024\2010\2010"
-Dataframes.dataframe_create(arquivo)
-
+dataset = Dataframes.create_dataset_by_files()
